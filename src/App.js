0a529a2bc245f55999d400cf5f3beed8eef232a7
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-03-22 09:34:03 +0000

    avance en componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const NavBar = () => {
+    return (
+        <div>
+            <p>ijdbfvsuhvus</p>
+        </div>
+    )
+}
+
+export default NavBar;
