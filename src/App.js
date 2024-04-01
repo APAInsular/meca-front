4387d75c111f8606d2c5cca6668a14f88d508e82
MadMatas <164335636+MadMatas@users.noreply.github.com>
2024-04-01 14:57:42 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import NavBar from './components/NavBar.jsx';
+import Author from './components/Author.jsx';
+import Obrasdest from './components/Obras_destacadas.jsx';
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
-   <div>
-    <p>Test</p>
-   </div>
+    <>
+    <NavBar/>
+    <Author/>
+    <Obrasdest/>
+    <Footer />
+    </>
   );
 }
 
