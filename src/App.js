1d54f75a99b6c7395ab2fa6ff5e11ed7245401b5
MadMatas <164335636+MadMatas@users.noreply.github.com>
2024-04-01 14:46:08 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+   <div>
+    <p>Test</p>
+   </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './components/NavBar.jsx';
+import Author from './components/Author.jsx';
+import Obrasdest from './components/Obras_destacadas.jsx';
+import Footer from './components/Footer.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar/>
+    <Author/>
+    <Obrasdest/>
+    <Footer />
   </React.StrictMode>
 );
 
