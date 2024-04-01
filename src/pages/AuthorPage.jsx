fdb5d517f9065501b9db46392c417a663ebc3669
MadMatas <164335636+MadMatas@users.noreply.github.com>
2024-04-01 15:07:29 +0300

    update

---
+++ src/pages/AuthorPage.jsx
@@ -0,0 +1,15 @@
+import NavBar from './components/NavBar.jsx';
+import Author from './components/Author.jsx';
+import Obrasdest from './components/Obras_destacadas.jsx';
+import Footer from './components/Footer.jsx'
+
+
+
+const AuthorPage = () => {
+    <div>
+    <NavBar/>
+    <Author/>
+    <Obrasdest/>
+    <Footer />
+    </div>
+}
