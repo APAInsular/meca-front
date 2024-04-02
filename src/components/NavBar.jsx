b3e4b9f1495225451ca20ecf0342bb60d12967b4
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-04-02 22:27:03 +0100

    intento de navbar funcional

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,108 +1,61 @@
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import React from 'react';
-import { Fragment } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-
-import "../styles/NavBar.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
 
 const navigation = [
-    { name: 'Inicio', path: '/', current: true },
-    { name: 'Acciones', path: '/acciones', current: true },
-    { name: 'Explorar', path: '/explorar', current: true },
-    { name: 'Blog', path: '/blog', current: true },
-    { name: 'Contáctanos', path: '/contactanos', current: true },
-    { name: 'Información', path: '/informacion', current: true },
+    { name: 'Inicio', path: '/' },
+    { name: 'Acciones', path: '/acciones' },
+    { name: 'Explorar', path: '/explorar' },
+    { name: 'Blog', path: '/blog' },
+    { name: 'Contáctanos', path: '/contactanos' },
+    { name: 'Información', path: '/informacion' },
 ];
 
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ');
-}
-
 export default function NavBar() {
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const handleClick = (itemName) => {
+        setSelectedItem((prevSelectedItem) => {
+            return prevSelectedItem === itemName ? null : itemName;
+        });
+    };
 
     return (
-        <Disclosure as="nav" className="border-bottom">
-            {({ open }) => (<>
-                <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 row p-3">
-                    <div className="relative flex h-16 items-center justify-between">
-                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            {/* Mobile menu button*/}
-                            <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                <span className="absolute -inset-0.5" />
-                                <span className="sr-only">Open main menu</span>
-                                {open ? (<XMarkIcon className="block h-6 w-6" aria-hidden="true" />) : (<Bars3Icon className="block h-6 w-6" aria-hidden="true" />)}
-                            </Disclosure.Button>
-                        </div>
-                        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                            <div className="flex flex-shrink-0 items-center">
-                                <NavLink to="/">
-                                    <img className="img-fluid"
-                                        style={{ maxWidth: '70px', maxHeight: '70px' }}
-                                        src="/LogoTradeTap.jpg" alt="Your Company" />
-                                </NavLink>
-                            </div>
-                            <div className="hidden sm:ml-6 sm:flex align-items-center">
-                                <div className="d-flex justify-content-start align-items-center">
-                                    {navigation.map((item) => (
-                                        <Link key={item.name} to={item.path} className="btn mx-3 color-button-general">
-                                            <strong>{item.name}</strong>
-                                        </Link>
-                                    ))}
+        <div className='my-3 px-3 d-flex align-items-center'>
+            <div className='col-md-1 justify-content-center'>
+                <img src="/Image/Logos/MECA-09.png" alt="Logo MECA" />
+            </div>
+            <div className="col-md-9 d-none d-sm-flex justify-content-start align-items-center">
+                {navigation.map((item) => (
+                    <div key={item.name}>
+                        {item.name === 'Acciones' || item.name === 'Explorar' ? (
+                            <div onClick={() => handleClick(item.name)} className="mx-3 d-flex justify-content-center">
+                                <div className="btn mx-3 text-center" style={{ borderRadius: "20px", backgroundColor: '#263C5C' }}>
+                                    <strong className='text-white'>{item.name}</strong>
+                                </div>
+                                <div hidden={selectedItem !== item.name}>
+                                    <Link to={item.path} className="btn mx-3 mt-2 d-flex justify-content-center" style={{ backgroundColor: '#263C5C', borderRadius: "20px" }}>
+                                        <strong className='text-white'>Logros</strong>
+                                    </Link>
+                                    <Link to={item.path} className="btn mx-3 mt-2 d-flex justify-content-center" style={{ backgroundColor: '#263C5C', borderRadius: "20px" }}>
+                                        <strong className='text-white'>Eventos</strong>
+                                    </Link>
+                                    <Link to={item.path} className="btn mx-3 mt-2 d-flex justify-content-center" style={{ backgroundColor: '#263C5C', borderRadius: "20px" }}>
+                                        <strong className='text-white'>Clasificación</strong>
+                                    </Link>
                                 </div>
                             </div>
-                        </div>
-                        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                            <NavLink to={"/registerWorker"} className="btn mx-3 color-button-general">
-                                <strong>Únete a nosotros</strong>
-                            </NavLink>
-
-                            <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                <span className="absolute -inset-1.5" />
-                                <span className="sr-only">View notifications</span>
-                                <BellIcon className="h-6 w-6" aria-hidden="true" />
-                            </button>
-
-                            {/* Profile dropdown */}
-                            <Menu as="div" className="relative ml-3">
-                                <div>
-                                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                        <span className="absolute -inset-1.5" />
-                                        <span className="sr-only">Open user menu</span>
-                                        <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                                    </Menu.Button>
-                                </div>
-                                <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
-                                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                        <Menu.Item>
-                                            {({ active }) => (
-                                                <>
-                                                    <Link to={`/`} className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                                                        Mi Perfil
-                                                    </Link>
-                                                    <Link className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                                                        Cerrar Sesión
-                                                    </Link>
-                                                </>
-                                            )}
-                                        </Menu.Item>
-                                    </Menu.Items>
-                                </Transition>
-                            </Menu>
-                        </div>
+                        ) : (
+                            <Link to={item.path} className="btn mx-3 d-flex justify-content-center" style={{ backgroundColor: '#263C5C', borderRadius: "20px" }}>
+                                <strong className='text-white'>{item.name}</strong>
+                            </Link>
+                        )}
                     </div>
-                </div>
-
-                <Disclosure.Panel className="sm:hidden">
-                    <div className="space-y-1 px-2 pb-3 pt-2">
-                        {navigation.map((item) => (<NavLink key={item.name} to={item.path} className={({ isActive }) => classNames(isActive
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium')}>
-                            {item.name}
-                        </NavLink>))}
-                    </div>
-                </Disclosure.Panel>
-            </>)}
-        </Disclosure>
+                ))}
+            </div>
+            <div className='col-md-2 text-right'>
+                <img src="/Image/PerfilPorDefecto.png" alt="PerfilPorDefecto" style={{ width: "100%", maxWidth: "45px", height: "auto" }} />
+            </div>
+        </div>
     );
 }
--- src/components/Obras_destacadas.jsx
+++ src/components/Obras_destacadas.jsx
@@ -1,16 +1,14 @@
 import '../styles/Obras.css'
-import firstObra from './Photos/Obra.jpg'
-
 
 function Obrasdest() {
     return (
         <>
             <div className="parent">
                 <div className="div obraTitle">OBRAS DESTACADAS</div>
-                <div className="div6"><img className="obraPng" src={firstObra} alt="Obra" /></div>
-                <div className="div7"><img className="obraPng" src={firstObra} alt="Obra" /></div>
-                <div className="div8"><img className="obraPng" src={firstObra} alt="Obra" /></div>
-                <div className="div9"><img className="obraPng" src={firstObra} alt="Obra" /></div>
+                <div className="div6"><img className="obraPng" src={'/Image/Obra.jpg'} alt="Obra" /></div>
+                <div className="div7"><img className="obraPng" src={'/Image/Obra.jpg'} alt="Obra" /></div>
+                <div className="div8"><img className="obraPng" src={'/Image/Obra.jpg'} alt="Obra" /></div>
+                <div className="div9"><img className="obraPng" src={'/Image/Obra.jpg'} alt="Obra" /></div>
             </div>
         </>
     );
