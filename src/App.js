ba38366b6d31cf62ef4a48a915778deb3a62eb24
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-04-08 14:00:18 +0100

    work of monday 08-04

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-//import ContactUs from './components/ContactUs';
-//import RegisterPage from './components/RegisterPage';
-//import MonumentFiltradosPage from './pages/MonumentFiltradosPage.jsx'
 import NavBar from './components/NavBar';
-//import MainContent from './components/MainContent';
-import InfoPage from './pages/InfoPage'
 import Footer from './components/Footer';
-import './App.css';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
-
     <div className="App m-0 p-0">
       <NavBar />
-      <InfoPage />
+      <MainContent />
       <Footer />
     </div>
   );
