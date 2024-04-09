942d35dbc22c6b0d6f4f00df568cff43ee3c10e4
MadMatas <164335636+MadMatas@users.noreply.github.com>
2024-04-09 16:44:31 +0300

    update

--- src/components/EventMonumentPhoto1.jsx
+++ src/components/EventMonumentPhoto1.jsx
@@ -5,7 +5,7 @@ import '../styles/EventMonumentPhoto1.css';
 const EventMonumentPhoto = () => {
     return(
         <div className='Monument1'>
-            <img className="first-monument-pic Element2" src="/Image/Obra.jpg" alt="MonumentPhoto" />
+            <img  className="first-monument-pic Element2" src="/Image/Obra.jpg" alt="MonumentPhoto" />
         </div>
     );
 
--- src/components/MonumentRoute1.jsx
+++ src/components/MonumentRoute1.jsx
@@ -3,9 +3,17 @@ import '../styles/MonumentRoute1.css';
 
 
 const MonumentRoute1 = () => {
+    const popOutImage = () => {
+        let image = document.getElementById("popOutImage");
+        let overlay = document.getElementById("overlay");
+    
+        image.classList.toggle("enlarged");
+        overlay.style.display = image.classList.contains("enlarged") ? "block" : "none";
+    }
     return(
         <div className='Route'>
-            <img className="Element3" src="/Image/Map.png" alt="MapImg" />
+            <img id="popOutImage" onClick={popOutImage} className="Element3 popOutImage" src="/Image/Map.png" alt="MapImg" />
+            <div id="overlay"></div>
         </div>
     );
 }
