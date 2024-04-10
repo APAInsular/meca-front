4b803a154dd41fbfc226bfb533e91ea22778f8cf
Gabriel <strazakklaku57@gmail.com>
2024-04-10 22:20:04 +0100

    Edited

---
+++ src/pages/RankingPage.jsx
@@ -0,0 +1,208 @@
+import '../styles/RankingPage.css';
+import user from '../images/user.svg';
+
+const RankingPage = () => {
+    return (
+        <div className="RankingPage">
+            <div className="top">
+                <h1>TOP</h1>
+                
+            </div>
+            <div className="users">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+
+
+            <div className="top2">
+                <h1>PLATINO</h1>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+
+
+            <div className="top">
+                <h1>ORO</h1>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+
+
+            <div className="top3">
+                <h1>PLATA</h1>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+
+
+            <div className="top2">
+                <h1>BRONCE</h1>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <div className="users2">
+                <img className='user' src={user} alt=""/>
+                <h3>Nombre Usuario</h3>
+                <h3 class="right">Nº Estatuas</h3>
+            </div>
+            <br></br><br></br>
+
+            
+        </div>        
+    )
+}
+
+export default RankingPage;
