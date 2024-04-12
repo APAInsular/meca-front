66c0e29aab7320f00971c03f2b843bf2ded8c0d2
MadMatas <164335636+MadMatas@users.noreply.github.com>
2024-04-12 13:54:57 +0300

    update

--- src/pages/UnEventPage.jsx
+++ src/pages/UnEventPage.jsx
@@ -1,15 +1,20 @@
 import MonumentRoute1 from "src/components/MonumentRoute1";
+import Participants from "src/components/Participants";
 
 
 const UnEventPage = () => {
-    return(
+    return (
         <div>
             <div className="max-w-[20%] flex float-left ml-7 mt-7 h-full border-solid border-8 border-[#263C5C]">
-        <MonumentRoute1 />
+                <MonumentRoute1 />
             </div>
-            <div className="justify-center align-middle pt-7 ml-5 relative">
-                <h6 className="flex text-3xl bg-[#263C5C] w-[30%] text-[#FFFFFF]">Evento Monumento</h6>
+            <div className="align-middle pt-7 pl-10 flex">
+                <h6 className="flex text-3xl bg-[#263C5C] w-[30%] text-[#FFFFFF] justify-center h-[50px] align-middle">Evento Monumento</h6>
+                <div className="relative ml-[5%]">
+                    <Participants />
+                </div>
             </div>
+
         </div>
     );
 }
