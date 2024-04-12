4016db241af451a23327cc60c21d04fa303f5ba0
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-04-12 10:17:18 +0100

    add ranking

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -17,7 +17,6 @@ import { Route, Routes } from 'react-router'
 import RegisterPage from '../pages/RegisterPage'
 import ListPage from '../pages/ListPage'
 import EntradaPage from '../pages/EntradaPage'
-import LoginPage from '../pages/LoginPage'
 
 const MainContent = () => {
     return (
@@ -42,7 +41,6 @@ const MainContent = () => {
             <Route element={<RankingPage />} path="/clasificacion"></Route>
             <Route element={<InfoPage />} path="/info"></Route>
             <Route element={<RegisterPage />} path="/registro"></Route>
-            <Route element={<LoginPage />} path="/login">    </Route>
         </Routes >
     )
 }
