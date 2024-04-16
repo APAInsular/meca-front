c194fcb4fd1fad993f2ac748ac90dad3ae6aff3f
BAN4NEQ <strazakklaku57@gmail.com>
2024-04-16 14:58:24 +0100

    Update

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,9 @@
 
 import React, { useState } from 'react';
 import '../styles/RegisterPage.css';
-import meca from '../images/MECA-09.png';
+import meca from '../images/meca-mobile.svg';
+import google from '../images/google.svg';
+import face from '../images/facebook.svg';
 
 const RegisterPage = () => {
   const [formData, setFormData] = useState({
@@ -13,21 +15,24 @@ const RegisterPage = () => {
 
   return (
     <div className='register-page'>
-      <div class="logo">
-          <img className='meca' src={meca} alt=""/>
+      <div class="logoa">
+          <img className='mecaa' src={meca} alt=""/>
       </div>
 
       <div class="register_form">
           <h5>REGISTRATE</h5>
-          <input type="text" placeholder='Nombre' class="nombre"></input>
-          <input type="text" placeholder='Apelidos' class="apelidos"></input><br></br>
+          <input type="text" placeholder='Nombre' class="nombre1"></input>
+          <input type="text" placeholder='Apelidos' class="apelidos1"></input><br></br>
           <input type="text" placeholder='Email' class="email"></input><br></br>
-          <input type="text" placeholder='Cludad' class="nombre"></input>
-          <input type="text" placeholder='Pais' class="apelidos"></input><br></br>
+          <input type="text" placeholder='Cludad' class="apelidos2"></input>
+          <input type="text" placeholder='Pais' class="nombre2"></input><br></br>
           <input type="text" placeholder='Nacionalidad' class="email"></input><br></br>
-          <input type="text" placeholder='Contraseña' class="nombre"></input>
-          <input type="text" placeholder='Repetir Contraseña' class="apelidos"></input><br></br>
-          <button>Entar</button><br></br>
+          <input type="text" placeholder='Contraseña' class="apelidos3"></input>
+          <input type="text" placeholder='Repetir Contraseña' class="nombre3"></input><br></br>
+          <button class="entar">Entar</button><br></br>
+          <button className='googleb'><img className='google' src={google} alt=""/></button>
+          <button className="facebookb"><img className='facebook' src={face} alt=""/></button>
+          <br></br><br></br><br></br><br></br>
           
       </div>
     </div>
