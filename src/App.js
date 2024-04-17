7d8c6e5b530f0d163989885d1e95bb7edffa3766
MadMatas <164335636+MadMatas@users.noreply.github.com>
2024-04-17 13:16:51 +0300

    LukasPull

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Footer from './components/Footer';
-
-
-
-//import MainContent from './components/MainContent';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
