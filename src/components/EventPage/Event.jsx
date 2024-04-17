a99d88ed7b0c02dd4a2c315151103b4af6fbc62a
MadMatas <164335636+MadMatas@users.noreply.github.com>
2024-04-17 13:14:25 +0300

    update

--- src/components/EventPage/Event.jsx
+++ src/components/EventPage/Event.jsx
@@ -28,10 +28,10 @@ const Event = ({ event }) => {
             </button>
             {expanded && (
                 <div className='hiddenContent'>
-                    <div className='Monument1 w-[270px] lg:w-[370px] mt-5 xl:ml-[43%] lg:ml-[43%] customFix-position'>
+                    <div className='Monument1 w-[270px] lg:w-[370px] mt-5 customFix-position'>
                         <EventMonumentPhoto />
                     </div>
-                    <div className='w-[270px] ml-[43%] Route lg:w-[70%] customFix-position'>
+                    <div className='w-[270px] Route lg:w-[70%] customFix-position'>
                         <MonumentRoute1 />
                     </div>
                     <div className='Profile customFix-position2 lg:ml-6'>
