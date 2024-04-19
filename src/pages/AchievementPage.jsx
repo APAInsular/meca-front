0ac5d8b4fab4470d8b354fd8b914b5383ca44189
BAN4NEQ <strazakklaku57@gmail.com>
2024-04-19 13:51:57 +0100

    AchivementPage update

--- src/pages/AchievementPage.jsx
+++ src/pages/AchievementPage.jsx
@@ -1,7 +1,20 @@
 import '../styles/Achivement.css';
+import star from '../images/star.svg'
 
 const AchivementPage = () => {
-    
+    return (
+        <div className='AchivementPage'>
+            <div class='progres'>
+                <img src={star} alt="/" style={{width: "40px", height: "40px"}} className='star'/>
+                <h3 className='logros'>Logros</h3>
+                <div className='line'>
+                    <div className='lineinside'>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default AchivementPage;
