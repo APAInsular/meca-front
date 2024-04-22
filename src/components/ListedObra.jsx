1c69decdd7e031cb88b7cd45e4c88cad5a9d43b2
MadMatas <164335636+MadMatas@users.noreply.github.com>
2024-04-22 15:33:55 +0300

    update

--- src/components/ListedObra.jsx
+++ src/components/ListedObra.jsx
@@ -1,21 +1,22 @@
 
 
 
-const ListedObra = ({obra, id}) => {
-// let titulo = "Escarel do carocol";
-// let estilo = "Baroco";
-// let discursoLink = "https://google.com"
-const { title, author, style } = obra;
-    return(
-        <div className="flex justify-between my-auto flex-shrink items-center flex-wrap">
-            <div>#{id}</div>
-            <div>{title}</div>
-            <div>{style}</div>
-           <button type="button" class="btn">Ubicacion</button>
-            <a href=""><img src='/Image/newspaper-folded.png' alt="newspapper" /></a>
-            <button type="button" class="btn"><img src="/Image/pen.png" alt="edit" /></button>
-            <button type="button" class="btn"><img src="/Image/multiply.png" alt="cancel" /></button>
-        </div>
+const ListedObra = ({ obra, id }) => {
+    // let titulo = "Escarel do carocol";
+    // let estilo = "Baroco";
+    // let discursoLink = "https://google.com"
+    const { title, author, style } = obra;
+    return (
+            <div className="flex justify-evenly my-auto flex-shrink items-center flex-wrap py-4">
+                <div>#{id}</div>
+                <div className="w-[170px]">{title}</div>
+                <div>{author.name}</div>
+                <div>{style}</div>
+                <button type="button"><img src="/Image/add.png" alt="" /></button>
+                <a href=""><img className="w-[50px] h-[30px]" src='/Image/newspaper-folded.png' alt="newspapper" /></a>
+                <button className="w-[50px] h-[30px]" type="button"><img className="w-[50px] h-[30px]" src="/Image/pen.png" alt="edit" /></button>
+                <button className="w-[50px] h-[30px]" type="button"><img className="w-[50px] h-[30px]" src="/Image/multiply.png" alt="cancel" /></button>
+            </div>
     );
 }
 
