1af9f01c9aef34cf777661becdca25f0dcf2b5c1
GR33NA5 <81330312+GR33NA5@users.noreply.github.com>
2024-04-24 09:01:46 +0100

    MonumentPage

--- src/pages/MonumentPage.jsx
+++ src/pages/MonumentPage.jsx
@@ -5,7 +5,10 @@ const AuthorPage = () => {
   return (
     <div className="row">
       <div className="d-flex">
-        <div className="ml-[15%] col-xs-12 col-md-6 col-lg-6" style={{ width: "30%" }}>
+        <div
+          className="ml-[15%] col-xs-12 col-md-6 col-lg-6"
+          style={{ width: "30%" }}
+        >
           <h1 className="">Escultura Móvil</h1>
           <img
             className="w-[55%] h-auto mt-3 mx-auto"
@@ -24,15 +27,21 @@ const AuthorPage = () => {
         </div>
         <div className="col-xs-12 col-md-6 col-lg-6">
           <h1 className="mx-auto">Autor</h1>
-          <img className="w-[40%] h-auto mt-2 mx-auto"
-          src="/Image/Cesar.jpg" alt="Cesar" />
+          <img
+            className="w-[40%] h-auto mt-2 mx-auto"
+            src="/Image/Cesar.jpg"
+            alt="Cesar"
+          />
           <p className="mx-auto">
             César Manrique (Arrecife, 24 abr 1919 - Teguise, 25 sep 1992)
             Pintor, escultor y artista canario 
           </p>
           <h1 className="mx-auto">Ubicación</h1>
-          <img className="w-[50%] h-auto mt-2 mx-auto"
-          src="/Image/map.png" alt="map" />
+          <img
+            className="w-[50%] h-auto mt-2 mx-auto"
+            src="/Image/map.png"
+            alt="map"
+          />
           <p className="mx-auto">
             Parque infantil del Castillo Negro Av. la Constitución 38003 Santa
             Cruz de Tenerife
