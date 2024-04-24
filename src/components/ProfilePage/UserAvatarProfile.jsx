bf3209a4061d7b20de9fef89154f71efe9850231
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-04-24 13:40:05 +0100

    perfil

---
+++ src/components/ProfilePage/UserAvatarProfile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const UserAvatarProfile = () => {
+    return (
+        <div className="row">
+            <div className="col-6 d-flex">
+                <div className="m-2" style={{ border: "1px solid black" }}>
+                    <img src="https://www.codedesign.org/storage/app/media/uploaded-files/1595519748783.jpeg" alt="" style={{ width: "150px", height: "150px" }} />
+                </div>
+                <div className="m-2" style={{ border: "1px solid black" }}>
+                    <img src="https://www.codedesign.org/storage/app/media/uploaded-files/1595519748783.jpeg" alt="" style={{ width: "150px", height: "150px" }} />
+                </div>
+                <div className="m-2" style={{ border: "1px solid black" }}>
+                    <img src="https://www.codedesign.org/storage/app/media/uploaded-files/1595519748783.jpeg" alt="" style={{ width: "150px", height: "150px" }} />
+                </div>
+            </div>
+            <div className="col-4">
+
+            </div>
+        </div>
+    )
+}
+
+export default UserAvatarProfile
---
+++ src/components/ProfilePage/UserInfoProfile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserInfoProfile = () => {
+    return (
+        <>
+            <h2>
+                Informacion
+            </h2>
+        </>
+    )
+}
+
+export default UserInfoProfile
