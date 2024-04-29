a8e7efc8c134272f145caae4cd96ee3ca63aabaf
BAN4NEQ <strazakklaku57@gmail.com>
2024-04-29 12:14:21 +0100

    Added translations for languages EN,ES,FR,IL,PL,DE in .json files

--- src/index.js
+++ src/index.js
@@ -5,12 +5,52 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+import global_es from "./translations/es/global.json";
+import global_en from "./translations/en/global.json";
+import global_de from "./translations/de/global.json";
+import global_fr from "./translations/fr/global.json";
+import global_it from "./translations/it/global.json";
+import global_pl from "./translations/pl/global.json";
+import global_jp from "./translations/jp/global.json";
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+
+i18next.init({
+  interpolation: {escapeValue: false},
+  lng: "es",
+  resources: {
+    es: {
+      global: global_es
+    },
+    en: {
+      global: global_en
+    },
+    de: {
+      global: global_de
+    },
+    fr: {
+      global: global_fr
+    },
+    it: {
+      global: global_it
+    },
+    pl: {
+      global: global_pl
+    },
+    jp: {
+      global: global_jp
+    }
+  }
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <I18nextProvider i18n={i18next}>
+        <App />
+      </I18nextProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
