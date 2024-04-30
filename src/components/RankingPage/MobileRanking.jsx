c8f07edf8a284976f1439d4cd58ab84a1b514730
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-04-30 13:45:42 +0100

    hol

--- src/components/RankingPage/MobileRanking.jsx
+++ src/components/RankingPage/MobileRanking.jsx
@@ -1,62 +1,63 @@
+import React, { useEffect, useState } from "react";
 
-import React from "react";
-import avatar from '../../images/usuario_avatar.png';
+import axios from "axios";
+
+import BigSpinner from "../BigSpinner";
+import UserRanking from "./UserRanking";
 
 const MobileRanking = () => {
-    const users = [
-        { name: "Nombre Usuario", statues: "Nº Estatuas" },
-        { name: "Nombre Usuario", statues: "Nº Estatuas" },
-        { name: "Nombre Usuario", statues: "Nº Estatuas" },
-        { name: "Nombre Usuario", statues: "Nº Estatuas" },
-        { name: "Nombre Usuario", statues: "Nº Estatuas" },
-        { name: "Nombre Usuario", statues: "Nº Estatuas" },
-        { name: "Nombre Usuario", statues: "Nº Estatuas" },
-    ];
+    const [data, setData] = useState([]);
 
-    return (
-        <div>
-            <div className="col-12 text-center p-2 h1 text-white" style={{backgroundColor: "#263c5c", margin: "0px"}}>PLATINA</div>
-            {users.map((user, index) => (
-                <div key={index} style={{backgroundColor: "#DEE6F0"}} className='d-flex align-items-center justify-content-between p-1 border-bottom border-black'>
-                    <div className="d-flex align-items-center justify-content-center">
-                        <img src={avatar} alt="usuario avatar" className="" style={{width: "40px", height: "auto"}}/>
-                        {user.name}
-                    </div>
-                    <div className="estatuas">{user.statues}</div>
-                </div>
-            ))}
-            <div className="col-12 text-center p-2 h1 text-white" style={{backgroundColor: "#263c5c", margin: "0px"}}>ORO</div>
-            {users.map((user, index) => (
-                <div key={index} style={{backgroundColor: "#DEE6F0"}} className='d-flex align-items-center justify-content-between p-1 border-bottom border-black'>
-                    <div className="d-flex align-items-center justify-content-center">
-                        <img src={avatar} alt="usuario avatar" className="" style={{width: "40px", height: "auto"}}/>
-                        {user.name}
-                    </div>
-                    <div className="estatuas">{user.statues}</div>
-                </div>
-            ))}
-            <div className="col-12 text-center p-2 h1 text-white" style={{backgroundColor: "#263c5c", margin: "0px"}}>PLATA</div>
-            {users.map((user, index) => (
-                <div key={index} style={{backgroundColor: "#DEE6F0"}} className='d-flex align-items-center justify-content-between p-1 border-bottom border-black'>
-                    <div className="d-flex align-items-center justify-content-center">
-                        <img src={avatar} alt="usuario avatar" className="" style={{width: "40px", height: "auto"}}/>
-                        {user.name}
-                    </div>
-                    <div className="estatuas">{user.statues}</div>
-                </div>
-            ))}
-            <div className="col-12 text-center p-2 h1 text-white" style={{backgroundColor: "#263c5c", margin: "0px"}}>BRONCE</div>
-            {users.map((user, index) => (
-                <div key={index} style={{backgroundColor: "#DEE6F0"}} className='d-flex align-items-center justify-content-between p-1 border-bottom border-black'>
-                    <div className="d-flex align-items-center justify-content-center">
-                        <img src={avatar} alt="usuario avatar" className="" style={{width: "40px", height: "auto"}}/>
-                        {user.name}
-                    </div>
-                    <div className="estatuas">{user.statues}</div>
-                </div>
-            ))}
-        </div>
-    );
+    const getUsersByPointsCategory = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/users/points-category');
+            // Manejar la respuesta
+            console.log(response.data);
+
+            setData(response.data);
+
+            // Aquí puedes actualizar el estado de tu aplicación o realizar otras operaciones con los datos recibidos
+        } catch (error) {
+            // Manejar errores
+            console.error('Error al obtener los usuarios por categoría de puntos:', error);
+        }
+    };
+
+    useEffect(() => {
+        getUsersByPointsCategory();
+    }, []);
+
+    if (data.length == 0) {
+        return (
+            <BigSpinner />
+        );
+    } else {
+
+        return (
+            <div>
+                <div className="col-12 text-center p-2 h1 text-white" style={{ backgroundColor: "#263c5c", margin: "0px" }}>TOP</div>
+                {data.top.map((user, index) => (
+                    <UserRanking user={user} key={index} />
+                ))}
+                <div className="col-12 text-center p-2 h1 text-white" style={{ backgroundColor: "#263c5c", margin: "0px" }}>PLATINO</div>
+                {data.platino.map((user, index) => (
+                    <UserRanking user={user} key={index} />
+                ))}
+                <div className="col-12 text-center p-2 h1 text-white" style={{ backgroundColor: "#263c5c", margin: "0px" }}>ORO</div>
+                {data.oro.map((user, index) => (
+                    <UserRanking user={user} key={index} />
+                ))}
+                <div className="col-12 text-center p-2 h1 text-white" style={{ backgroundColor: "#263c5c", margin: "0px" }}>PLATA</div>
+                {data.plata.map((user, index) => (
+                    <UserRanking user={user} key={index} />
+                ))}
+                <div className="col-12 text-center p-2 h1 text-white" style={{ backgroundColor: "#263c5c", margin: "0px" }}>BRONCE</div>
+                {data.bronce.map((user, index) => (
+                    <UserRanking user={user} key={index} />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default MobileRanking
